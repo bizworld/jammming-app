db88a0e55f40a9bd5227c37fe36c9bc95203c0a7
bizworld <bizworld@outlook.com>
2017-12-28 10:59:47 -0500

    Fix errors.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App'; // OR './App.js';
 import registerServiceWorker from './registerServiceWorker';
-import './components/App/App.js'; //import App from './components/App/App.js'; ?
+import App from './components/App/App'; //import App from './components/App/App.js'; ?
 import './components/App/App.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
