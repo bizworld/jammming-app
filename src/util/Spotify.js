ca5f796491d0264211a199648a24dccd6b2e3b33
bizworld <bizworld@outlook.com>
2017-12-30 11:09:50 -0500

    Work on a Spotify Access Token.

---
+++ src/util/Spotify.js
@@ -0,0 +1,25 @@
+// Declare an empty variable that will hold the user's access token.
+const userAccessToken = '';
+
+// Create a Spotify module, first as an empty object.
+const Spotify = {
+  getAccessToken: () => {
+    /* Check if the user's access token is already set. If it is, return the
+    value saved to access token. */
+    if (userAccessToken) { // to check if userAccessToken is set? 78/99, 2nd p
+      return userAccessToken;
+    } else if (URL) { // check the URL to see if it has just been obtained (if not already set)
+      // see 79/99 for details
+    } else if () { // 80/99 and 81/99
+
+    }
+  }
+
+};
+
+
+
+
+
+// export the Spotify module to expose it to other components of the Jammming app.
+export default Spotify;
