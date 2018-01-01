2ba5407fb0143b9846b07ad3e9c35e19980da68d
bizworld <bizworld@outlook.com>
2018-01-01 10:51:07 -0500

    Update the Spotify.js file.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,6 @@
-// Declare an empty variable that will hold the user's access token.
-const userAccessToken = '';
+/* Declare an empty variable that will hold the user's access token.
+/ `let` instead of `const` because the value of userAccessToken is liable to change */ 
+let userAccessToken; // defined (but not initialized) i.e. a variable that will be used later
 
 // Create a Spotify module, first as an empty object.
 const Spotify = {
@@ -7,7 +8,7 @@ const Spotify = {
     /* Check if the user's access token is already set. If it is, return the
     value saved to access token. */
     if (userAccessToken) { // to check if userAccessToken is set? 78/99, 2nd p
-      return userAccessToken;
+      return userAccessToken; // paused here!
     } else if (URL) { // check the URL to see if it has just been obtained (if not already set)
       // see 79/99 for details
     } else if () { // 80/99 and 81/99
